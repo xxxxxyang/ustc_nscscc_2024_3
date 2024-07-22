61dd057f3a0dff7c8ede55596ec0711d8d60a611
rxliu <lrx123@mail.ustc.edu.cn>
2024-07-22 18:02:41 +0800

    add dispatch module and some inst_pack config

---
+++ src/main/scala/Dispatch/Dispatch.scala
@@ -0,0 +1,24 @@
+import chisel3._
+import chisel3.util._
+import InstPacks._
+
+class Dispatch(n: Int) extends Module {
+  val io = IO(new Bundle{
+    val inst_pack = Input(Vec(2,new pack_RN))
+    //the element of 2 alu
+    val elem = Input(Vec(2,UInt(log2Ceil(n).W)))
+    //selct which function unit to go
+    val inst_valid = Output(Vec(4,Vec(2,Bool())))
+  })
+
+  val queue_hit = Wire (Vec(2,UInt(4.W)))
+  for(i <- 0 until 2){
+    queue_hit(i) := UIntToOH(Mux(io.inst_pack(i).fu_id(2).asBool, Mux(io.elem(1) <= io.elem(2), 1.U(2.W), 0.U(2.W)), io.inst_pack(i).fu_id(1,0))) & Fill(4, io.inst_pack(i).inst_valid)
+  }
+
+  for(i <- 0 until 4){
+    for(j <- 0 until 2){
+        io.inst_valid(i)(j) := queue_hit(j)(i)
+    }
+  }
+}
--- src/main/scala/InstPacks.scala
+++ src/main/scala/InstPacks.scala
@@ -9,4 +9,24 @@ object InstPacks {
         val pred_npc        = UInt(32.W)
         val exception       = UInt(8.W)
     }
+    class pack_ID extends pack_PD{
+        val rj              = UInt(5.W)
+        val rk              = UInt(5.W)
+        val rd              = UInt(5.W)
+        val rd_valid        = Bool()
+        val imm             = UInt(32.W)
+        val alu_op          = UInt(4.W)
+        val alu_rs1_sel     = UInt(1.W)
+        val alu_rs2_sel     = UInt(2.W)
+        val br_type         = UInt(4.W)
+        val mem_type        = UInt(5.W)
+        val priv_vec        = UInt(13.W)
+        val fu_id           = UInt(3.W)
+    }
+    class pack_RN extends pack_ID{
+        val prj             = UInt(log2Ceil(PREG_SIZE).W)
+        val prk             = UInt(log2Ceil(PREG_SIZE).W)
+        val prd             = UInt(log2Ceil(PREG_SIZE).W)
+        val pprd            = UInt(log2Ceil(PREG_SIZE).W)
+    }
 }
