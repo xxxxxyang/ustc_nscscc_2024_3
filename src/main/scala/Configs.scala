a399a8b4e4b193f23a355058a10fb9be41279b09
rxliu <lrx123@mail.ustc.edu.cn>
2024-07-30 16:14:40 +0800

    add csr

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -24,3 +24,21 @@ object Predict_Config {
     val ICALL     = 3.U(2.W)
     val ELSE      = 0.U(2.W)
 }
+object exception_code{
+    val INT     = 0x00.U(7.W) // interrupt
+    val PIL     = 0x01.U(7.W) // page illegal load
+    val PIS     = 0x02.U(7.W) // page illegal store
+    val PIF     = 0x03.U(7.W) // page illegal fetch
+    val PME     = 0x04.U(7.W) // page maintain exception
+    val PPI     = 0x07.U(7.W) // page privilege illegal
+    val ADEF    = 0x08.U(7.W) // address exception fetch
+    val ADEM    = 0x48.U(7.W) // address exception memory
+    val ALE     = 0x09.U(7.W) // address align exception
+    val SYS     = 0x0b.U(7.W) // system call
+    val BRK     = 0x0c.U(7.W) // breakpoint
+    val INE     = 0x0d.U(7.W) // instruction not exist
+    val IPE     = 0x0e.U(7.W) // instruction privilege exception
+    val FPD     = 0x0f.U(7.W) // floating point disable
+    val FPE     = 0x12.U(7.W) // floating point exception
+    val TLBR    = 0x3F.U(7.W) // TLB refill
+}
