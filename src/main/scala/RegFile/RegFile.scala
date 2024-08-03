80c10794fb03b8bd199edeb1ff49dc70ebb60244
rxliu <lrx123@mail.ustc.edu.cn>
2024-08-03 20:40:43 +0800

    fix freelist

--- src/main/scala/RegFile/RegFile.scala
+++ src/main/scala/RegFile/RegFile.scala
@@ -17,20 +17,20 @@ class RegFile extends Module{
         val we        = Input(Vec(4, Bool()))
     })
     val rf = RegInit(VecInit.fill(PREG_SIZE)(0.U(32.W)))
-
+    val we = VecInit.tabulate(4)(i => io.we(i) && io.prd(i).orR)
     for(i <- 0 until 4){
-        when(io.we(i)){
+        when(we(i)){
             rf(io.prd(i)) := io.wdata(i)
         }
     }
     for(i <- 0 until 4){
         // write first
         {
-            val w_hit = VecInit.tabulate(4)(j => io.we(j) && io.prd(j) === io.prj(i)).asUInt
+            val w_hit = VecInit.tabulate(4)(j => we(j) && io.prd(j) === io.prj(i)).asUInt
             io.prj_data(i) := Mux(w_hit.orR, Mux1H(w_hit, io.wdata), rf(io.prj(i)))
         }
         {
-            val w_hit = VecInit.tabulate(4)(j => io.we(j) && io.prd(j) === io.prk(i)).asUInt
+            val w_hit = VecInit.tabulate(4)(j => we(j) && io.prd(j) === io.prk(i)).asUInt
             io.prk_data(i) := Mux(w_hit.orR, Mux1H(w_hit, io.wdata), rf(io.prk(i)))
         }
     }
